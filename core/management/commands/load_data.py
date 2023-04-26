1555e252ffa24c328dfe9380b25c57503c43373a
Chanrley Ferreira <35461240+chanrley@users.noreply.github.com>
2023-04-26 11:51:19 -0300

    Dropdown chained finalizado
    Tutorial explicativo https://www.youtube.com/watch?v=YXzb4tw2IrI

--- core/management/commands/load_data.py
+++ core/management/commands/load_data.py
@@ -1,8 +1,48 @@
 from django.core.management.base import BaseCommand
+from core.models import Course, Module
 
 
 class Command(BaseCommand):
     help = 'Load Courses and Modules'
 
     def handle(self, *args, **kwargs):
-        raise NotImplementedError
+        Module.objects.all().delete()
+        course_names = [
+            'Computer Science', 'Mathematics'
+        ]
+
+        if not Course.objects.count():
+            for course_name in course_names:
+                    Course.objects.create(name=course_name)
+        #CS
+        cs = Course.objects.get(name='Computer Science')
+        compsci_modules = [
+             'AI',
+             'Machine Learning',
+             'Web Development',
+             'Software Engineering',
+             'NoSQL Databases'
+        ]
+        
+        for module in compsci_modules:
+             Module.objects.create(name=module, course=cs)
+        
+        #Maths
+        math = Course.objects.get(name="Mathematics")
+        math_modules = [
+             'Linear Algebra',
+             'Differential Equations',
+             'Graph Theory',
+             'Topology',
+             'Number Theory'
+        ]
+        for module in math_modules:
+             Module.objects.create(name=module, course=math)
+        
+        
+        
+        #Physics and so on
+
+
+
+        
